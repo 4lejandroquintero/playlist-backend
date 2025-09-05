5e50ee542bc3ed4029802a3c77f996e7ff9df09a
Alejandro Quintero Alzate <alejandro.quintero@robotmade.us>
2025-09-05 18:23:55 -0500

    feat: estructura inicial del backend

---
+++ src/main/java/com/playlist_backend/PlaylistBackendApplication.java
@@ -0,0 +1,13 @@
+package com.playlist_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistBackendApplication.class, args);
+	}
+
+}
